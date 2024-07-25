65c79c1c7187a7a61f368e8c53c9d97fea3bb475
creekcred <76488889+creekcred@users.noreply.github.com>
2024-07-25 13:26:55 -0700

    Update ConfigManager.cs

--- src/Utils/ConfigManager.cs
+++ src/Utils/ConfigManager.cs
@@ -5,7 +5,7 @@ namespace CNCPulse.Utils
 {
     public static class ConfigManager
     {
-        private const string ConfigFilePath = "config.json";
+        private const string ConfigFilePath = "AppSettings.json";
 
         public static AppConfig LoadConfig()
         {
@@ -14,7 +14,7 @@ namespace CNCPulse.Utils
                 string json = File.ReadAllText(ConfigFilePath);
                 return JsonConvert.DeserializeObject<AppConfig>(json);
             }
-            return new AppConfig();
+            return CreateDefaultConfig();
         }
 
         public static void SaveConfig(AppConfig config)
@@ -22,9 +22,44 @@ namespace CNCPulse.Utils
             string json = JsonConvert.SerializeObject(config, Formatting.Indented);
             File.WriteAllText(ConfigFilePath, json);
         }
+
+        private static AppConfig CreateDefaultConfig()
+        {
+            return new AppConfig
+            {
+                IsInitialized = false,
+                DefaultCompany = "CNC Pulse",
+                LogFilePath = "log.txt",
+                MachineConfigs = new List<MachineConfig>(),
+                ServerSettings = new ServerSettings(),
+                QuarantineFolder = "QuarantineFolder",
+                OpenAISettings = new OpenAISettings(),
+                PerplexitySettings = new PerplexitySettings(),
+                Branding = new BrandingSettings
+                {
+                    CompanyName = "CNC Pulse",
+                    LogoPath = "path/to/default_logo.png"
+                },
+                MESSettings = new MESSettings(),
+                ERPSettings = new ERPSettings()
+            };
+        }
     }
 
     public class AppConfig
     {
+        public bool IsInitialized { get; set; }
         public string DefaultCompany { get; set; }
-        public string Log
+        public string LogFilePath { get; set; }
+        public List<MachineConfig> MachineConfigs { get; set; }
+        public ServerSettings ServerSettings { get; set; }
+        public string QuarantineFolder { get; set; }
+        public OpenAISettings OpenAISettings { get; set; }
+        public PerplexitySettings PerplexitySettings { get; set; }
+        public BrandingSettings Branding { get; set; }
+        public MESSettings MESSettings { get; set; }
+        public ERPSettings ERPSettings { get; set; }
+    }
+
+    // ... (other classes remain the same)
+}
